53aa15c082246be283905df009407d7be7ad7407
Xiaowei <xiaowei.c.li@gmail.com>
2021-07-05 20:42:10 +0000

    downloaded ejs and set ejs as the templating engine/view engine

--- express_server.js
+++ express_server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//tells the Express app to use EJS as its templating engine
+app.set("view engine", "ejs");
+
+
+
 //object with urls in it 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
