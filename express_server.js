113540bc4752317138ab859e9e203a29d1e27ee7
Xiaowei <xiaowei.c.li@gmail.com>
2021-07-08 23:20:46 +0000

    debugged middleware again because i didn't have /logout whitelisted, meaning that when i logged out of registered accounts (vs hardcoded data) the cookie would remain. also adjusted helper functions and secured the edit and logout for urls so that it checks the url and the userID before allowing edits/deletions

--- express_server.js
+++ express_server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-var cookieParser = require('cookie-parser')
+let cookieParser = require('cookie-parser');
 
 //"installed it" in Express so it uses it
-app.use(cookieParser())
+app.use(cookieParser());
 
 //tells the Express app to use EJS as its templating engine
 app.set("view engine", "ejs");
@@ -22,63 +22,76 @@ const urlDatabase = {
     userID: "userRandomID"
   },
   "9sm5xK": {
-    longURL: "http://www.google.com", 
+    longURL: "http://www.google.com",
     userID: "user2RandomID"
   }
 };
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "1234"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "12345"
   }
-}
+};
 //---ROUTES------------------------------------------------------------------------
 
 //---LISTEN--------------------------------------------------------------------------------------
 //tells us when we have a connection to local server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});  
+});
 
 //--GET------------------------------------------------------------------------------
 
 //returns hello on "homepage" if end-point is "/"
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { 
   res.send("Hello!");
 });
 
-/*
-using middleware to check if the user is logged in 
+/* 
+using middleware to check if the user is logged in
 instead of doing an if statement for every single get request
 */
 app.use('/', (req, res, next) => {//app.use works for EVERYTHING (get, post)
-  const user = req.cookies.user_id; 
-  const whiteList = ['/urls', '/login', '/register'];//['/login', '/register'] gives me the error 
+  const userID = req.cookies.user_id; 
+  const whiteList = ['/urls', '/login', '/register', '/logout'];
   // console.log(req.path);
   // console.log(typeof req.path);
-  if (user || whiteList.includes(req.path) || req.path.startsWith('/u/')) {//check if we have user object
-    console.log("in the if condition");
+  if (users[userID]) { //check if we have userID (from cookie from registering) in our user database
+    // console.log("the user does exist")
     next(); //goes to next http request
+  } else if (whiteList.includes(req.path) || req.path.startsWith('/u/')) {
+    // console.log("the path is in the whitelist OR the path includes /u/")
+    next(); 
   } else {
-    console.log("in the else condition -- meaning not logged in")
-    res.redirect('/urls'); //if not, redirect to prompt (redirect to prompt gives me the error)
+    // console.log("user didn't exist, whitelist didn't include, path didn't have /u/")
+    res.redirect('/urls'); //if not, redirect to prompt 
   }
 });
 
 //if end-point is /urls, returns json string w urls in urlDatabase
 app.get("/urls", (req, res) => {
-  const templateVars = { 
-    urls: urlDatabase,
-    user: users[req.cookies.user_id] && users[req.cookies.user_id].email //check if ID exists then if it does it'll try to get the email
-  };
-  res.render("urls_index", templateVars);
+  if (users[req.cookies.user_id]) { //check if cookie user.ID exists in user database (should)
+    let loggedIn = urlsforUser(users[req.cookies.user_id]); //loggedIn = checked object that exists
+    // console.log(loggedIn);
+    const templateVars = {
+      urls: loggedIn,
+      user: users[req.cookies.user_id] && users[req.cookies.user_id].email //check if ID exists then if it does it'll try to get the email    
+    };
+    res.render("urls_index", templateVars);
+  } else {
+    const templateVars = {
+      errorMessage: "THIS ID DOESN'T EXIST IN THE USER DATABASE YOU FIEND",
+      user: null,
+    };
+    res.render("urls_error", templateVars);  
+  }
 });
 
 //allows header to be used and cookies to exist from header
@@ -86,14 +99,14 @@ app.get("/urls/new", (req, res) => {
   const templateVars = {
     user: users[req.cookies.user_id] && users[req.cookies.user_id].email //check if ID exists then if it does it'll try to get the email
   };
-  res.render("urls_new", templateVars);
+  res.render("urls_new", templateVars); 
 });
 
 //reponds to login form template
 app.get("/login", (req, res) => {
   const templateVars = {
     user: req.cookies.user_id,
-    password: req.body.password  
+    password: req.body.password
   };
   res.render("urls_login", templateVars);
 });
@@ -109,10 +122,10 @@ app.get("/register", (req, res) => {
 //new route to render infomation about urls
 app.get("/urls/:shortURL", (req, res) => {//note :shortURL is a "general" path
   //Use the shortURL from the route parameter to lookup it's associated longURL from the urlDatabase
-  const templateVars = { 
-    shortURL: req.params.shortURL, 
+  const templateVars = {
+    shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL,
-    user: users[req.cookies.user_id] && users[req.cookies.user_id].email //check if ID exists then if it does it'll try to get the email
+    user: urlsforUser(users[req.cookies.user_id]) && users[req.cookies.user_id].email //check if ID exists then if it does it'll try to get the email
   };
   res.render("urls_show", templateVars);
 });
@@ -122,10 +135,10 @@ app.get("/urls/:shortURL", (req, res) => {//note :shortURL is a "general" path
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL; //taking shortURL from browser
   const longURL = urlDatabase[shortURL];
-  if(longURL) {
+  if (longURL) {
     res.redirect(301, longURL.longURL);
   } else {
-    res.render("Error 403: The short URL doesn't exist")
+    res.render("Error 403: The short URL doesn't exist");
   }
 });
 
@@ -142,8 +155,8 @@ app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   const tempKey = generateRandomString(); //assigning temporary key
   urlDatabase[tempKey] = { // adding OBJECT to database
-    longURL: req.body.longURL, 
-    userID: req.cookies.user_id}; 
+    longURL: req.body.longURL,
+    userID: req.cookies.user_id};
   res.redirect(`/urls/${tempKey}`); //redirecting client to shortUrl
 });
 
@@ -152,8 +165,8 @@ app.post("/login", (req, res) => {
   if (userEmailChecker(req.body.email)) { //checking input email w database emails
     if (passwordChecker(req.body.password)) { //checking input pswd w database pswds
       //if matches, get matching ID and make a cookie called user_id
-      res.cookie('user_id', getUserID(req.body.email, req.body.password)); 
-      res.redirect('/urls');//redirects browser back to homepage 
+      res.cookie('user_id', getUserID(req.body.email, req.body.password));
+      res.redirect('/urls');//redirects browser back to homepage
     } else {
       res.status(403).send("Email was correct but password was not");
     }
@@ -163,43 +176,55 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const newUserID = generateRandomString(); //generates random ID for new users 
-  const newUser = { //setting info from register into an object
-    id: newUserID,
-    email: req.body.email,
-    password: req.body.password
-  };
-  const existingUser = userEmailChecker(newUser.email)
-  // console.log("existing user", existingUser);
-  if(!newUser.email || !newUser.password) {
+  const newUserID = generateRandomString(); //generates random ID for new users
+  const existingUser = userEmailChecker(req.body.email);
+  if (!req.body.email || !req.body.password) {
     res.status(400).send("Error 400: You left the email and or password blank");
   } else if (existingUser) {
     res.status(400).send("Error 400: Email already exists");
   } else {
-    users[newUserID] = newUser; //adding to new user object to user database 
-    res.cookie('user_id', newUser.id); //creating a cookie w new user's data
-    // console.log(users); //to show myself if the new ID is added to the object
-    res.redirect('/urls')//redirect back to homepage
+    users[newUserID] = { //setting info from register into an object
+      id: newUserID,
+      email: req.body.email,
+      password: req.body.password
+    };
+    console.log(users);
+    res.cookie('user_id', newUserID); //creating a cookie w new user's data
+    res.redirect('/urls');//redirect back to homepage
   }
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  urlDatabase[shortURL] = req.body.newlongURL;
-  res.redirect('/urls');//redirects to homepage 
+  const shortURL = req.params.shortURL; //saves url from page
+  const url = urlDatabase[shortURL]; //database object 
+  if (url.userID === req.cookies.user_id) { //checks if ID is in database 
+    url.longURL = req.body.newlongURL; //THE EDITING MAGIC
+    res.redirect('/urls');//redirects to homepage
+  } else {
+    res.send("ERROR: The user ID doesn't match... sus..");
+  }
 });
 
 //route to remove a URL resource
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;//takes shortURL from browser assigns to var
-  delete urlDatabase[shortURL];//deletes data based on the var 
-  res.redirect('/urls');//redirects to homepage 
+  if (urlDatabase[shortURL]) { //compare shortURL to shortURL in database
+    if (urlsforUser(users[req.cookies.user_id])) { //see if user ID from cookie matches one matching shortURL in users database
+      delete urlDatabase[shortURL];//deletes data based on the var
+      res.redirect('/urls');//redirects to homepage
+    } else {
+      res.status(403).send("ERROR: The user ID doesn't match... sus..");
+    }
+  } else {
+    res.status(403).send("ERROR: The shortURL doesn't match...");
+  }
 });
 
 //route to clear cookie
 app.post("/logout", (req, res) => {
+  // console.log("hitting logout");
   res.clearCookie('user_id');
-  res.redirect('/urls');//redirects to homepage 
+  res.redirect('/urls');//redirects to homepage
 });
 
 
@@ -216,17 +241,17 @@ const generateRandomString = function() {
     }
   }
   return randomString;
-}; 
+};
 
 const userEmailChecker = function(email) {//check if user email is in user object already
   for (const u in users) {//users is the data object
     const user = users[u];
     if (user.email === email) {
-      console.log(`${email}-----------`, user)   
+      // console.log(`${email}-----------`, user);
       return user;
     }
   }
-  return false; 
+  return false;
 };
 
 const passwordChecker = function(password) {//checks password against data
@@ -241,18 +266,24 @@ const passwordChecker = function(password) {//checks password against data
 const getUserID = function(email, password) {//to return checked ID for password & email
   let existingUserID = "";
   for (const u in users) {
-    if(users[u].email === email && users[u].password === password) {
+    if (users[u].email === email && users[u].password === password) {
       existingUserID = users[u].id;
     }
   }
   return existingUserID;
 };
 
-const compareIDwithDatabaseID = function(inputID) {
+const urlsforUser = function(inputID) {
+  let match = {};
   for (const u in urlDatabase) {
-    if (urlDatabase[u].userID === inputID) {
-      return true;
+    // console.log("inputID.id", inputID.id)
+    // console.log("userID from database", urlDatabase[u].userID)
+    if (urlDatabase[u].userID === inputID.id) {
+      // console.log("urldatabase[u]", urlDatabase[u])
+      // console.log("urldatabase", urlDatabase)
+      match[u] = urlDatabase[u];
+      return match;
     }
   }
   return false;
-}
+};
